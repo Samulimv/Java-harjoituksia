88a61a6e7bec96582d7584931e1b46342f146b17
Samulimv <t3vasa03@students.oamk.fi>
2024-09-22 17:04:51 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,5 +7,13 @@ public class Main {
         MacDialog macDialog = new MacDialog(128, 128, null);
         System.out.println(macDialog.getTruePixels());
         System.out.println("True Pixels (Mac): " + macDialog.getTruePixels());
+
+        Animal animal1 = new Cat("Katti");
+        Cat cat1 = new Cat("Kissa");
+        Horse horse1 = new Horse("Hevonen");
+
+        animal1.breathe();
+       cat1.breathe();
+       horse1.talk();
     }
 }
